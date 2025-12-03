d7a061055dfbd295a98da5eef5579a85846ff051
thanh <thanhhero765@gmail.com>
2025-12-03 16:00:46 +0700

    feat: add loading animation SVG and replace LinearProgress with SvgColor component

--- src/routes/sections.tsx
+++ src/routes/sections.tsx
@@ -2,13 +2,13 @@ import type { RouteObject } from 'react-router';
 
 import { lazy, Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
-import { varAlpha } from 'minimal-shared/utils';
 
 import Box from '@mui/material/Box';
-import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
 
 import { AuthLayout } from 'src/layouts/auth';
 import { DashboardLayout } from 'src/layouts/dashboard';
+
+import { SvgColor } from '../components/svg-color';
 
 // ----------------------------------------------------------------------
 
@@ -58,14 +58,7 @@ const renderFallback = () => (
       justifyContent: 'center',
     }}
   >
-    <LinearProgress
-      sx={{
-        width: 1,
-        maxWidth: 320,
-        bgcolor: (theme) => varAlpha(theme.vars.palette.text.primaryChannel, 0.16),
-        [`& .${linearProgressClasses.bar}`]: { bgcolor: 'text.primary' },
-      }}
-    />
+    <SvgColor src="/assets/icons/load-bubble.svg"/>
   </Box>
 );
 
